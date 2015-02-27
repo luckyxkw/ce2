101c65f3d04de28df59c9d174eb54ebb73d475b5
luckyxkw <cocolonel.x@gmail.com>
2015-02-28 03:20:50 +0800

    Final for CE2
    this is my final CE2

--- testSearch.java
+++ testSearch.java
@@ -1,14 +1,36 @@
 package ce2;
 
 import static org.junit.Assert.*;
+
 import org.junit.Test;
 
-public class testSearch {
-
+public class TestSearch {
+	
+	@Test
 	public void test() {
-		TextBuddy tb = new TextBuddy();
-		String output = tb.executeCommand("clear");
-		assertEquals("Your task list is empty", output);
+		String[] name = {"tasklist.txt"};
+		TextBuddy tb = TextBuddy.setUp(name);
+		
+		String output = null;
+		output = tb.executeCommand("search say");
+		assertEquals("1.say hi!\n2.say goodbye!\n", output);
+		
+		tb.executeCommand("add meeting at school");
+		tb.executeCommand("add play basketball");
+		tb.executeCommand("add meeting at Starbucks");
+		tb.executeCommand("add meeting at home");
+		tb.executeCommand("add play soccor");
+		tb.executeCommand("add play badminton");
+		
+		output = tb.executeCommand("search meeting");
+		assertEquals("1.meeting at school\n"
+				+ "2.meeting at Starbucks\n"
+				+ "3.meeting at home\n", output);
+		output = tb.executeCommand("search play");
+		assertEquals("1.play basketball\n"
+				+ "2.play soccor\n"
+				+ "3.play badminton\n", output);
+		
 	}
 
 }
